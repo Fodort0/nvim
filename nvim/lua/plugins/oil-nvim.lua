7acaad6fd0905a2222c0e5e50349d624810ebbde
Fodor Tamás <141072145+Fodort0@users.noreply.github.com>
2025-04-29 12:51:58 +0200

    Update oil-nvim.lua

--- nvim/lua/plugins/oil-nvim.lua
+++ nvim/lua/plugins/oil-nvim.lua
@@ -1,11 +1,18 @@
 return {
-	"stevearc/oil.nvim",
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	config = function()
-		require("oil").setup({
-			default_file_explorer = true,
-		})
-	end,
-	lazy = false,
-}
+  "stevearc/oil.nvim",
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+    })
 
+    -- Set Ctrl+C to function as Esc in Oil.nvim buffers
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "oil",
+      callback = function()
+        vim.keymap.set("n", "<C-c>", "<Esc>", { buffer = true, silent = true })
+      end,
+    })
+  end,
+  lazy = false,
+}
